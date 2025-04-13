15f9e22d3f3c706f99d64121e0f4b5e8416f9d1a
briteming <briteming@gmail.com>
2025-04-12 20:36:27 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
     repo            : 'moment',       // github 仓库名
     backgroundMusic      : '//music.163.com/song/media/outer/url?id=1500817509.mp3',
     // 背景音乐的绝对地址
-    // access_token : 'abcde'+'fghijk',       
+    access_token : 'github_pat_11AAM4DVY0LNu3oBcVwJLs_kbVl9eVvTpnO'+'c0aEymIHAm7JadCCdlDFsThLF35vrInYSNVBJREPnqV3Kvp',      
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     perPage        : '5'             // 默认一页显示几篇文章
 }
